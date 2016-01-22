63b4a8efd77c36b5cff36661e40559e732163ff7
MatthiasBusch <m.busch@cortona.de>
2016-01-22 15:25:50 +0100

    init commit

---
+++ config/Migrations/20160122140805_CreateProducts.php
@@ -0,0 +1,28 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateProducts extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('products');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('image', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
